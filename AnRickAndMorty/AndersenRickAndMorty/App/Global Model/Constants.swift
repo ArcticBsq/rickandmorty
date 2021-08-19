0d6beebfe21cec59fc0a8e95277130d66e042365
ArcticBsq <sqwerld@gmail.com>
2021-08-19 14:38:57 +0300

    Search functionality
    Добавлен searchBar, пока только по загруженному массиву, без подключения API.

--- AnRickAndMorty/AndersenRickAndMorty/App/Global Model/Constants.swift
+++ AnRickAndMorty/AndersenRickAndMorty/App/Global Model/Constants.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 enum ViewMetrics {
-    static let fontSize: CGFloat = 35.0
+    static let MainScreenButtonFontSize: CGFloat = 25.0
     static let spacing: CGFloat = 20.0
     static let corner: CGFloat = 15.0
     static let detailObjectFontSize: CGFloat = 20.0
