f7beb363a95fa26557a8f30eb00e5147770f8896
ArcticBsq <sqwerld@gmail.com>
2021-08-24 18:57:46 +0300

    Filter screen
    Начало положено

--- AnRickAndMorty/AndersenRickAndMorty/App/Global View/UIFabric.swift
+++ AnRickAndMorty/AndersenRickAndMorty/App/Global View/UIFabric.swift
@@ -64,6 +64,18 @@ class UIFabric {
         return view
     }
     
-    
+    func makeBarButton(_ target: Any?, action: Selector, imageName: String, size:CGSize = CGSize(width: 32, height: 32)) -> UIBarButtonItem {
+        let button = UIButton(type: .system)
+        
+        button.setImage(UIImage(named: imageName), for: .normal)
+        button.addTarget(target, action: action, for: .touchUpInside)
+
+        let menuBarItem = UIBarButtonItem(customView: button)
+        menuBarItem.customView?.translatesAutoresizingMaskIntoConstraints = false
+        menuBarItem.customView?.heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        menuBarItem.customView?.widthAnchor.constraint(equalToConstant: size.width).isActive = true
+
+        return menuBarItem
+    }
     
 }
